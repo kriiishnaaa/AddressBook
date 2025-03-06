2b130a555ede84795a6231d22e7a2f72d06ddada
Krishna <krishnaag866@gmail.com>
2025-03-06 17:42:15 +0530

    Set up of Address Book Application

---
+++ src/main/java/com/AddressBook/AdressBookApp/AdressBookAppApplication.java
@@ -0,0 +1,13 @@
+package com.AddressBook.AdressBookApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdressBookAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdressBookAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/AddressBook/AdressBookApp/AdressBookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AddressBook.AdressBookApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdressBookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
