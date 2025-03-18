e83ee6fcbe0cc84b10f5b9c8183c4f6fffbf816b
Krishna <krishnaag866@gmail.com>
2025-03-18 10:26:42 +0530

    Added DTO and Model

---
+++ src/main/java/com/example/AddressBookApp/DTO/AddressBookDTO.java
@@ -0,0 +1,11 @@
+package com.example.AddressBookApp.DTO;
+import lombok.*;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class AddressBookDTO {
+    private Long id;
+    private String name;
+    private String phone;
+}
---
+++ src/main/java/com/example/AddressBookApp/model/AddressBookModel.java
@@ -0,0 +1,18 @@
+package com.example.AddressBookApp.model;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "contacts")
+public class AddressBookModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String phone;
+    private String email;
+}
